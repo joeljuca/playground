75c291643f514458511e5724f5cee585f15d92ce
Joel Jucá <joelwallis@gmail.com>
2023-01-05 21:11:39 -0500

    chore: lein new lei

---
+++ lei/src/lei/core.clj
@@ -0,0 +1,6 @@
+(ns lei.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ lei/test/lei/core_test.clj
@@ -0,0 +1,7 @@
+(ns lei.core-test
+  (:require [clojure.test :refer :all]
+            [lei.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
