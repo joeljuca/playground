4415c8b43c715136551e2ff46d8e8373d49301e2
Joel Jucá <joelwallis@gmail.com>
2023-02-03 17:09:54 -0300

    feat(hey-python): a simple Hello World

---
+++ hey-python/src/hello.py
@@ -0,0 +1,7 @@
+# See: https://learnxinyminutes.com/docs/python/
+
+def greet(name, greeting="Hello"):
+    print(f"{greeting}, {name}!")
+
+greet("Joel")  # => Hello, Joel!
+greet("Joel", "Welcome")  # => Welcome, Joel!
