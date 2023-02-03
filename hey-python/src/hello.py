205de848c9216960aacae13de9f738f0af55025b
Joel Jucá <joelwallis@gmail.com>
2023-02-03 17:36:56 -0300

    style(hey-python): python3 -m black src

--- hey-python/src/hello.py
+++ hey-python/src/hello.py
@@ -1,7 +1,9 @@
 # See: https://learnxinyminutes.com/docs/python/
 
+
 def greet(name, greeting="Hello"):
     print(f"{greeting}, {name}!")
+
 
 greet("Joel")  # => Hello, Joel!
 greet("Joel", "Welcome")  # => Welcome, Joel!
